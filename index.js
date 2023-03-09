c0216f0430385bb96d8498486cca9bee112ceb23
claralindbloms <97987325+claralindbloms@users.noreply.github.com>
2023-03-09 15:12:57 +0100

    fixed code

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 
 
 document.querySelector("body").addEventListener("keypress", keypress);
-function keypress(event){
-    if(event.key == "a"){
+function keypress(event) {
 
-        let astronaut = document.querySelector(".astronaut-invisible");
-        astronaut.style.transition = 'all 0.5s linear 0s';
-        astronaut.style.opacity = 100;
+    if (event.key) {
+        var styleSheetssy = document.getElementsByTagName("link");
+        console.log(styleSheetssy);
+
+        let specificSheet = event.key + ".css";
+
+        getStyleSheet(specificSheet);
     }
+}
+
+function getStyleSheet(key) {
+    var styleSheetss = document.getElementsByTagName("link");
+    for (var i = 0; i < styleSheetss.length; i++) {
+        if (styleSheetss[i].attributes[1].nodeValue.includes(key)) {
+            console.log(key);
+            if (styleSheetss[i].attributes[1].nodeValue == key) {
+                styleSheetss[i].disabled = false;
+                
+                DisableAgain(i);
+            }
+        }
+
+    }
+}
+
+function DisableAgain(i) {
+
+    var styleSheetssy = document.getElementsByTagName("link");
+
+    setTimeout(() => {
+        styleSheetssy[i].setAttribute("disabled", true);
+    }, 3000);
+
 }
